acec3fad10dcc63ed33dfe2e27b75cade3e5b8b3
Rajesh Bavananatham <rajesh.bavan@gmail.com>
2020-08-15 21:26:20 +1000

    Adding Consul client support to register with consul server

--- install-node.sh
+++ install-node.sh
@@ -6,4 +6,7 @@ sudo apt update
 sudo apt install -y nodejs npm
 sudo node -v
 cd /usr/src/app/ && npm install
-
+sudo mv /tmp/weather-svc-v1-consul-client.json /opt/consul/config/
+sudo chown consul:consul /opt/consul/config/weather-svc-v1-consul-client.json
+sudo chmod 755 /opt/consul/config/weather-svc-v1-consul-client.json
+sudo ls -ltr /opt/consul/config/
